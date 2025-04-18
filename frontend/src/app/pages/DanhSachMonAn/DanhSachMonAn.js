d42cab98a963fa34e623892e19e81b7b5c581756
KasinMidair <lamvida2005@gmail.com>
2025-04-18 23:54:16 +0700

    feat:frontend_build basic mainroutes/navigation

---
+++ frontend/src/app/pages/DanhSachMonAn/DanhSachMonAn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DanhSachMonAn.css';
+
+function DanhSachMonAn() {
+  return (
+    <div className="danhsachmonan-page">
+      <h1>DanhSachMonAn Page</h1>
+    </div>
+  );
+}
+
+export default DanhSachMonAn;
