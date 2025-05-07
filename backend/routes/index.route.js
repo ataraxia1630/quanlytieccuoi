f31ab3a1da5571550002fbc2528b2741145d9e10
TuUyen038 <uyentu510@gmail.com>
2025-05-08 00:28:39 +0700

    feat: hoadon and phieudattiec be continuing

---
+++ backend/routes/index.route.js
@@ -0,0 +1,7 @@
+const phieuDatTiecRoute = require("./phieudattiec.route");
+const hoaDonRoute = require("./hoadon.route");
+
+module.exports = (app) => {
+ app.use("/phieudattiec", phieuDatTiecRoute);
+ app.use("/phieudattiec/:id", hoaDonRoute);
+}
