492b8ea431fb662222256acf415a965911d1270c
nhittyn <hudinhdong@gmail.com>
2025-05-18 22:59:40 +0700

    chore: add validation

--- backend/routes/loaisanh.route.js
+++ backend/routes/loaisanh.route.js
@@ -1,5 +1,10 @@
 const { Router } = require('express');
 const { LoaiSanhController } = require('../controllers/loaisanh.controller');
+const validate = require('../middlewares/validation');
+const {
+  createLoaiSanhValidation,
+  updateLoaiSanhValidation,
+} = require('../validations/loaisanh.validation');
 const router = Router();
 
 // Lấy tất cả loại sânh
@@ -9,10 +14,20 @@ router.get('/', LoaiSanhController.getAllLoaiSanh);
 router.get('/:id', LoaiSanhController.getLoaiSanhById);
 
 // Thêm loại sânh mới
-router.post('/', LoaiSanhController.createLoaiSanh);
+router.post(
+  '/',
+  createLoaiSanhValidation,
+  validate,
+  LoaiSanhController.createLoaiSanh
+);
 
 // Cập nhật loại sânh theo id
-router.put('/:id', LoaiSanhController.updateLoaiSanh);
+router.put(
+  '/:id',
+  updateLoaiSanhValidation,
+  validate,
+  LoaiSanhController.updateLoaiSanh
+);
 
 // Xóa loại sânh theo id
 router.delete('/:id', LoaiSanhController.deleteLoaiSanh);
