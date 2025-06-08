68250ef6d406001f43a031728d66d03d6600aada
nhittyn <hudinhdong@gmail.com>
2025-06-08 13:43:44 +0700

    chore: format heading

--- frontend/src/app/pages/PhanQuyen/PhanQuyen.js
+++ frontend/src/app/pages/PhanQuyen/PhanQuyen.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box, Grid, TextField, Button, Typography } from '@mui/material';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function PhanQuyen() {
-  return <div>PHAN QUYEN</div>;
+  return (
+    <Box sx={{ p: 3 }}>
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
+      <Typography
+        variant="h4"
+        sx={{ fontWeight: 'bold', color: '#063F5C', mb: 4 }}
+      >
+        Phân quyền
+      </Typography>
+      <Box></Box>
+    </Box>
+  );
 }
