7489ab7ea6be7b89a41bd7bde1a5d66de0afd245
nhittyn <hudinhdong@gmail.com>
2025-06-12 23:12:14 +0700

    feat: add getAll pers

--- frontend/src/app/service/quyen.service.js
+++ frontend/src/app/service/quyen.service.js
@@ -1,27 +1,63 @@
 const baseURL = '/api/permission';
 const QuyenService = {
   getPerOfUser: async () => {
-    const token = localStorage.getItem('accessToken');
-    if (!token) {
-      throw new Error('Có lỗi xảy ra. Vui lòng đăng nhập lại!');
-    }
-    let uri = baseURL;
-
-    const res = await fetch(uri, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    if (!res.ok) {
-      const error = await res.json();
-      if (res.status === 401) {
-        localStorage.removeItem('accessToken');
-        throw new Error('Phiên đăng nhập hết hạn. Vui lòng đăng nhập lại!');
+    try {
+      const token = localStorage.getItem('accessToken');
+      if (!token) {
+        throw new Error('Có lỗi xảy ra. Vui lòng đăng nhập lại!');
       }
-      throw new Error(error.message || 'Không thể lấy danh sách quyền!');
+      let uri = baseURL;
+
+      const res = await fetch(uri, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (!res.ok) {
+        const error = await res.json();
+        if (res.status === 401) {
+          localStorage.removeItem('accessToken');
+          throw new Error('Phiên đăng nhập hết hạn. Vui lòng đăng nhập lại!');
+        }
+        throw new Error(error.message || 'Không thể lấy danh sách quyền!');
+      }
+      const result = await res.json();
+      return result.permissions;
+    } catch (error) {
+      throw new Error(
+        error.message || 'Đã có lỗi xảy ra. Vui lòng thử lại sau.'
+      );
     }
-    const result = await res.json();
-    return result.permissions;
+  },
+
+  getAll: async () => {
+    try {
+      const token = localStorage.getItem('accessToken');
+      if (!token) {
+        throw new Error('Có lỗi xảy ra. Vui lòng đăng nhập lại!');
+      }
+      let uri = baseURL + '/all';
+
+      const res = await fetch(uri, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (!res.ok) {
+        const error = await res.json();
+        if (res.status === 401) {
+          localStorage.removeItem('accessToken');
+          throw new Error('Phiên đăng nhập hết hạn. Vui lòng đăng nhập lại!');
+        }
+        throw new Error(error.message || 'Không thể lấy danh sách quyền!');
+      }
+      const result = await res.json();
+      return result.permissions;
+    } catch (error) {
+      throw new Error(
+        error.message || 'Đã có lỗi xảy ra. Vui lòng thử lại sau.'
+      );
+    }
   },
 };
 
