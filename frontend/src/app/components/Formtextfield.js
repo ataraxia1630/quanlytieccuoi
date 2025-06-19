40be575bfb03b0cc990efa01d55c4248c0d0d3d9
nhittyn <hudinhdong@gmail.com>
2025-06-19 13:23:47 +0700

    feat: update helperText in FormTextField styling and enhance BaoCaoThang report display

--- frontend/src/app/components/Formtextfield.js
+++ frontend/src/app/components/Formtextfield.js
@@ -1,32 +1,39 @@
-import { TextField } from "@mui/material";
+import { TextField } from '@mui/material';
 
 const FormTextField = ({ label, value, onChange, ...props }) => (
   <TextField
     label={label}
-    value={value ? value : ""}
+    value={value ? value : ''}
     onChange={onChange}
     fullWidth
     variant="outlined"
     sx={{
-      "& .MuiOutlinedInput-root": {
-        "& fieldset": {
-          borderColor: "#063F5C",
+      '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+          borderColor: '#063F5C',
         },
-        "&:hover fieldset": {
-          borderColor: "#063F5C",
+        '&:hover fieldset': {
+          borderColor: '#063F5C',
         },
-        "&.Mui-focused fieldset": {
-          borderColor: "#063F5C",
+        '&.Mui-focused fieldset': {
+          borderColor: '#063F5C',
         },
-        "& input": {
-          color: "black",
+        '& input': {
+          color: 'black',
         },
       },
-      "& .MuiInputLabel-root": {
-        color: "black",
+      '& .MuiInputLabel-root': {
+        color: 'black',
       },
-      "& .MuiInputLabel-root.Mui-focused": {
-        color: "#063F5C",
+      '& .MuiInputLabel-root.Mui-focused': {
+        color: '#063F5C',
+      },
+
+      '& .MuiFormHelperText-root': {
+        whiteSpace: 'normal', // Cho phép xuống dòng nếu helperText dài
+        overflow: 'hidden', // Ẩn phần văn bản vượt quá
+        textOverflow: 'ellipsis', // Thêm dấu ... nếu văn bản quá dài
+        maxWidth: '150px', // Giới hạn chiều rộng của helperText
       },
     }}
     {...props}
