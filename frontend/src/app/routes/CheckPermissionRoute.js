22e38f799ca54a421b81e3547272fc6709754d7c
nhittyn <hudinhdong@gmail.com>
2025-06-20 16:31:07 +0700

    fix: remove account after logout

--- frontend/src/app/routes/CheckPermissionRoute.js
+++ frontend/src/app/routes/CheckPermissionRoute.js
@@ -1,16 +1,20 @@
 import { usePermission } from '../../context/PermissionContext';
 import { Navigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
 export default function CheckPermissionRoute({
   children,
   requiredPermissions,
 }) {
   const { permissions } = usePermission();
+  const { logout } = useAuth();
 
   const hasPermission = permissions.some((p) =>
     requiredPermissions.includes(p)
   );
 
   if (!hasPermission) {
+    localStorage.removeItem('permissions');
+    logout();
     return <Navigate to="/signin" replace />;
   }
 
