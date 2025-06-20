fcba8a83bf7d5c276cf871de2a05e8a10bb47660
nhittyn <hudinhdong@gmail.com>
2025-06-20 15:42:58 +0700

    style: format RangeInputs component

--- frontend/src/app/components/Rangeinput.js
+++ frontend/src/app/components/Rangeinput.js
@@ -1,4 +1,5 @@
-import { Box, TextField, Typography } from "@mui/material";
+import { Box, TextField, Typography } from '@mui/material';
+import NumericFormatCustom from './NumericFormatCustom';
 
 const RangeInputs = ({
   label,
@@ -10,14 +11,14 @@ const RangeInputs = ({
 }) => {
   const inputSx = {
     width: width,
-    "& .MuiOutlinedInput-root": {
-      fontSize: "0.95rem",
-      height: "37.5px",
-      "& input": {
-        padding: "7px 12px",
+    '& .MuiOutlinedInput-root': {
+      fontSize: '0.95rem',
+      height: '37.5px',
+      '& input': {
+        padding: '7px 12px',
       },
-      "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-        borderColor: "#063F5C",
+      '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+        borderColor: '#063F5C',
       },
     },
   };
@@ -27,7 +28,7 @@ const RangeInputs = ({
       <Typography variant="subtitle1" sx={{ mb: 1 }}>
         {label}:
       </Typography>
-      <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
         <TextField
           variant="outlined"
           size="small"
@@ -35,8 +36,11 @@ const RangeInputs = ({
           value={fromValue}
           onChange={(e) => onFromChange(e.target.value)}
           sx={inputSx}
+          InputProps={{
+            inputComponent: NumericFormatCustom,
+          }}
         />
-        <Box sx={{ width: "10px", borderTop: "1.2px solid #ccc", mb: 1.5 }} />
+        <Box sx={{ width: '10px', borderTop: '1.2px solid #ccc', mb: 1.5 }} />
         <TextField
           variant="outlined"
           size="small"
@@ -44,6 +48,9 @@ const RangeInputs = ({
           value={toValue}
           onChange={(e) => onToChange(e.target.value)}
           sx={inputSx}
+          InputProps={{
+            inputComponent: NumericFormatCustom,
+          }}
         />
       </Box>
     </Box>
