98201e81e530de620529c258d95a2b3c2f1ed00e
TuUyen038 <uyentu510@gmail.com>
2025-06-22 02:15:30 +0700

    add edit hoa don

--- backend/routes/hoadon.route.js
+++ backend/routes/hoadon.route.js
@@ -4,6 +4,6 @@ const hoadonController = require('../controllers/hoadon.controller');
 router.get("/:id", hoadonController.index)
 router.post("/create", hoadonController.create)
 router.delete("/:id", hoadonController.delete)
-router.post("/restore-dichvu/:soPhieu", hoadonController.restoreDichVu);
-
+router.put("/:id", hoadonController.update);
+router.get("/check-date/:id", hoadonController.checkDate)
 module.exports = router;
